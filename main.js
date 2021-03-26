ed8ad22484c2428c373b35a7cc0733545e68e3ce
Hongdonggeon <ghd700@naver.com>
2021-03-26 15:50:35 +0900

    solve

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const sign_btn = document.querySelector(".sign");
+const sign__lists=document.querySelectorAll(".sign__item");
+
+
+sign_btn.onmouseout = signout;
+sign_btn.onmouseover = signopen;
+
+
+
+function signopen(){
+   sign__lists.forEach(function(item,index,arr) {
+      item.style.animation="sign-on 0.5s linear forwards";
+      item.style.display="block";
+   })
+}
+
+function signout(){
+   sign__lists.forEach(function(item,index,arr) {
+      item.style.animation="sign-on 0.5s linear reverse";
+      item.style.display="none";
+   })
+}
